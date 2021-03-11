eb36b3130e625c2342cb797bee5c7ee8e90fd975
sgw8 <meeskeev@gmail.com>
2021-03-12 01:20:23 +0200

    added reducer and action for user login

--- mern/src/store.js
+++ mern/src/store.js
@@ -4,18 +4,27 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-
+import {userLoginReducer} from './reducers/userReducer.js'
 
 const reducer = combineReducers({
      productList: productListReducer,
      productDetails: productDetailsReducer,
-     cart: cartReducer
+     cart: cartReducer,
+     userLogin: userLoginReducer
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+// получаем items из локал сторедж для товаров
+const cartItemsFromStorage = localStorage.getItem('cartItems') 
+? JSON.parse(localStorage.getItem('cartItems')) : []
 
+// получаем items из localstorage для юзеров
+const userInfoFromStorage = localStorage.getItem('userinfo') 
+? JSON.parse(localStorage.getItem('userInfo')) : null
+
+// origin state
 const initialState = {
-     cart: {cartItems: cartItemsFromStorage}
+     cart: {cartItems: cartItemsFromStorage},
+     userLogin: {userInfo: userInfoFromStorage}
 }
 
 const middleware = [thunkMiddleware]
