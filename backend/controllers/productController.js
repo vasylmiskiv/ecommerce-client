e91f3bad7a1619eb19b20ab4a0a99236ab227803
sgw8 <meeskeev@gmail.com>
2021-03-23 23:06:37 +0200

    added create & update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,8 +20,6 @@ const getProductById = asyncHandler(async(req, res) => {
     }
 })
 
-
-
 //delete product
 //route delete /api/products/:id
 //Admin
@@ -38,7 +36,65 @@ const deleteProduct = asyncHandler(async(req, res) => {
     }
 })
 
+//create product
+//route post /api/products
+//Admin
+const createProduct = asyncHandler(async(req, res) => {
+   const product = new Product ({
+       name: 'HOCO C12 Smart Dual USB Micro Cable',
+       price: 0,
+       user: req.user._id,
+       image: '/images/charger.jpg',
+       brand: 'Sample brand',
+       category: 'sample category',
+       countInStock: 0,
+       numReviews: 0,
+       description: 'Sample description'
+   })
+   
+   const createdProduct = await product.save()
+   res.status(201).json(createdProduct)
+})
+
+//change product
+//route put /api/products/:id
+//Admin
+const updateProduct = asyncHandler(async(req, res) => {
+   const {
+        name,
+        price, 
+        description,
+        image,
+        brand,
+        category,
+        countInStock
+    } = req.body
+    
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image =  image
+    product.brand =  brand
+    product.category = category
+    product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+       res.status(404) 
+       throw new Error('Product not found')
+    }
+ })
 
 export {
-    getProductById, getProducts, deleteProduct
+    getProductById, 
+    getProducts,
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
+
+
